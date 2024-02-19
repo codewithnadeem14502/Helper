0e2c1fde49ef6e17bedeac55471605cba4644aab
codewithnadeem14502 <codewithnadeem@14502>
2024-02-20 00:51:25 +0530

    add routing (api)

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -3,30 +3,29 @@ import styles from "./singlePost.module.css";
 
 import { Suspense } from "react";
 import PostUser from "@/components/PostUser/PostUser";
+import { getPost } from "@/lib/data";
+// FETECH DATA WITH AN API
+// const GetSinglePost = async (slug) => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
 
-const GetSinglePost = async (slug) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
-
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
-  return res.json();
-};
+//   if (!res.ok) {
+//     throw new Error("Something went wrong");
+//   }
+//   return res.json();
+// };
 const SinglePage = async ({ params }) => {
   const { slug } = params;
   console.log("aaaaaaaa ", slug);
-  const singlePost = await GetSinglePost(slug);
+  // const singlePost = await GetSinglePost(slug);
+  const post = await getPost(slug);
   return (
     <div className={styles.container}>
       {/* {post.img && ( */}
-      <div className={styles.imgContainer}>
-        <Image
-          src="https://images.pexels.com/photos/792326/pexels-photo-792326.jpeg?auto=compress&cs=tinysrgb&w=600"
-          alt=""
-          fill
-          className={styles.img}
-        />
-      </div>
+      {post.img && (
+        <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img} />
+        </div>
+      )}
       {/* )} */}
       <div className={styles.textContainer}>
         <h1 className={styles.title}>{singlePost.title}</h1>
@@ -37,9 +36,9 @@ const SinglePage = async ({ params }) => {
           </div>
         </div> */}
 
-        <Suspense fallback={<div>Loading...</div>}>
+        {/* <Suspense fallback={<div>Loading...</div>}>
           <PostUser userId={singlePost.userId} />
-        </Suspense>
+        </Suspense> */}
         <div className={styles.content}>{singlePost.body}</div>
       </div>
     </div>
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -6,23 +6,19 @@ const PostCard = ({ post }) => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/792326/pexels-photo-792326.jpeg?auto=compress&cs=tinysrgb&w=600"
-            alt=""
-            fill
-            className={styles.img}
-          />
-        </div>
-
+        {post.img && (
+          <div className={styles.imgContainer}>
+            <Image src={post.img} alt="" fill className={styles.img} />
+          </div>
+        )}
         <span className={styles.date}>
-          {/* {post.createdAt?.toString().slice(4, 16)} */} 110:23434
+          {post.createdAt?.toString().slice(4, 16)}
         </span>
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>{post?.title}</h1>
-        <p className={styles.desc}>{post?.body}</p>
-        <Link className={styles.link} href={`/blog/${post?.id}`}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
+        <Link className={styles.link} href={`/blog/${post.slug}`}>
           READ MORE
         </Link>
       </div>
